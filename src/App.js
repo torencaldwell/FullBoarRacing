c1e0cc6c3a291c4a209e2dec1bff86572ef9ce16
Toren Caldwell <torencaldwell@gmail.com>
2018-11-09 11:23:58 -0600

    fixed some mqtt stuff

--- src/App.js
+++ src/App.js
@@ -8,30 +8,6 @@ import mqtt from 'mqtt';
 
 class App extends Component {
 
-
-  componentDidMount() {
-    var client = mqtt.connect('ws://localhost', {
-      port: 8080,
-      reconnectPeriod: 5000
-    });
-
-    console.log(client);
-
-    client.on('connect', () => {
-      client.subscribe('device', (err) => {
-        if(!err){
-          console.log("Sending");
-          client.publish('device', 'Testing');
-        }
-      });
-    });
-
-    client.on('message', (topic, message) => {
-      console.log(message.toString());
-      client.end()
-    })
-  }
-
   render() {
     return (
         <div className="App">
--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import colors from '../res/colors';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import mqtt from 'mqtt';
 
 class Dashboard extends Component {
 
-  render(){
-    return(
-      <Link to="/dashboard">This is the dashboard</Link>
+  state = {
+    incoming : ""
+  };
 
+  bin2string(array) {
+    var result = "";
+    for(var i=0; i< array.length; i++){
+      result += String.fromCharCode(array[i]);
+    }
+    return result;
+  }
+
+  componentDidMount() {
+    var client = mqtt.connect('ws://10.42.0.166', {
+      port: 8080,
+      reconnectPeriod: 5000
+    });
+
+    client.on('connect', () => {
+      console.log("MQTT CONNECTED!");
+
+      client.subscribe('device', () => {
+        console.log("subscribed to device");
+      })
+    });
+
+
+    client.on('message', function(topic, message) {
+      this.setState({incoming: this.bin2string(message)});
+    }.bind(this));
+  }
+
+
+  render() {
+    return(
+      <p>{this.state.incoming}</p>
     );
   }
 };
